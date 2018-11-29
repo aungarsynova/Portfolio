0ec3544896d12ed67a75c30d732efedfe0abf0d2
unknown <you@example.com>
2018-11-29 15:48:54 -0500

    DATABASE WOOOORKED

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const vm = new Vue({
+    el : "#text-main",
+    data : {
+       mainText: " "
+        
+    },
+
+    created : function() {
+        this.getData(null);
+    },
+
+    methods: {
+        getData(e) {
+            //debugger;
+            let targetURL = `./includes/connect.php`;
+
+            fetch(targetURL)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                //run a function to parse our data
+                this.showTextData(data[0]);//run a fucntion to put the data on the page
+            })
+            .catch(function(error) {
+                console.error(error);
+
+            });
+        },
+        show() {
+           //debugger;    
+           this.show=true; 
+        },
+        
+    showTextData(data) {
+        //change VM content here and make it show up on the page
+       this.mainText = data.text
+      
+     }
+    }
+});
+
+function parseTextData(text) {
+    //destructure thed database info and grab just what we need
+    const { text } = mainText;
+    //take the database data and put it on the page
+    document.querySelector(".input").textContent = text;
+    
+};
